296656ed466959d292321a36aefc58da5d9ddfb3
and-is <andispaudel@gmail.com>
2024-05-14 18:54:22 +0545

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 //require("dotenv").config({ path: "./env" });
-
 import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-  path: "./env",
+  path: "/home/andis/Documents/chai/.env",
 });
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed! ", err);
+  });
 
 /*
 import express from "express";
